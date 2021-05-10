3e01b3f84a656355760aac0f5e53b3d244d0cc27
dant98x00000001 <dant98@gmail.com>
2021-05-09 20:18:56 -0400

    Added Sound

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame  # Provides PyGame framework
 import random  # Used for randomization
 from ballmgr import Ball
 
+import os, sys
+APP_FOLDER = os.path.dirname(os.path.realpath(sys.argv[0]))
+
 # Set Constants
 WIDTH = 800  # Screen Width
 HEIGHT = 600  # Screen Height
@@ -9,11 +12,29 @@ WINDOW_SIZE = (WIDTH, HEIGHT)  # Window Size tuple
 BACKGROUND_COLOR = (0, 255, 0)  # Background Color
 DIFFICULTY = 3  # Number of Balls to juggle
 
+def getsounds():
+    """generate list of sounds"""
+    blaster_sounds = list()
+    blaster_folder = os.path.join(APP_FOLDER, "blaster_sounds")
+    for file in [os.path.join(blaster_folder, f) for f in os.listdir(blaster_folder)]:
+        print(file)
+        blaster_sounds.append(pygame.mixer.Sound(file))
+    fail_sounds = list()
+    fail_folder = os.path.join(APP_FOLDER, "fail_sounds")
+    for file in [os.path.join(fail_folder, f) for f in os.listdir(fail_folder)]:
+        print(file)
+        fail_sounds.append(pygame.mixer.Sound(file))
+    gameover = pygame.mixer.Sound("imperial_march_rock.mp3")
+    return blaster_sounds, fail_sounds, gameover
+
+
 def main():
     pygame.init()  # Initialize the PyGame environment
     screen = pygame.display.set_mode(WINDOW_SIZE)  # Set screen size
     clock = pygame.time.Clock()  # Create Clock for managing time
     background_image = pygame.transform.scale(pygame.image.load("galaxy_background.jpg"), WINDOW_SIZE)
+    # Sounds
+    blaster_sounds, fail_sounds, gameover_sound = getsounds()
 
     # Create Ball Objects.  Three for your average juggler
     balls = []  # Initialize List of balls in play
@@ -34,6 +55,7 @@ def main():
                 # Process any events captured by pygame
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     # React to mouse click event
+                    pygame.mixer.Sound.play(random.choice(blaster_sounds))
                     for ball in balls:
                         # Run check on each ball in play
                         if ball.rect.collidepoint(pygame.mouse.get_pos()):
@@ -54,11 +76,19 @@ def main():
                     ball.update()
                 if not ball.alive:
                     # If the ball was dropped, remove it from the list
+                    pygame.mixer.Sound.play(random.choice(fail_sounds))
                     del balls[i]
 
             if len(balls) <= 1:
                 # We dropped all but one ball, which is too easy to juggle.
                 # Or we quit the game (zero balls)
+                pygame.mixer.Sound.play(gameover_sound)
+                QUIT = False
+                while pygame.mixer.get_busy() and not QUIT:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            QUIT = True
+                    pass
                 gameover = True  # Game over
 
             if score != prevscore and score % 10 == 0:
@@ -67,7 +97,7 @@ def main():
         else:
             # Get events occuring while the window is not in focus
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT or QUIT:
                     # If there is a QUIT event (user clicked close)
                     balls = []  # Remove all but one the remaining balls
 
